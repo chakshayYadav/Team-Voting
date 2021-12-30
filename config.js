bd733851ba7abd9f805544b202a58de516242df6
chakshayYadav <74128864+chakshayYadav@users.noreply.github.com>
2021-12-30 18:18:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+apiKey: "AIzaSyA38olKbAP8meZ8wYOpHR-IYzUuHq36YWQ",
+  authDomain: "team-vote-8a126.firebaseapp.com",
+  databaseURL: "https://team-vote-8a126-default-rtdb.firebaseio.com",
+  projectId: "team-vote-8a126",
+  storageBucket: "team-vote-8a126.appspot.com",
+  messagingSenderId: "1023954516311",
+  appId: "1:1023954516311:web:c817840c5b9c0c7e946612"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
